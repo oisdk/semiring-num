1f02ffd9e55edad7b8e9f5405eb4991fcfed5f51
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-01-30 20:16:30 +0000

    Template Haskell

--- src/Data/Semiring/TH.hs
+++ src/Data/Semiring/TH.hs
@@ -3,22 +3,43 @@ module Data.Semiring.TH where
 import Control.Monad
 import Language.Haskell.TH
 
-cmbN :: Int -> Q Exp -> Q Exp
-cmbN n f' = do
-  f <- f'
-  xs <- replicateM n (newName "x")
-  ys <- replicateM n (newName "y")
-  let args = [TupP (map VarP xs), TupP (map VarP ys)]
-      ntup = TupE (zipWith (AppE . AppE f) (map VarE xs) (map VarE ys))
-  pure (LamE args ntup)
+repN :: Int -> String -> Q Dec
+repN n nm = do
+    let v = VarP (mkName nm)
+    rhs <- TupE <$> replicateM n (pure (VarE (mkName nm)))
+    return $ ValD v (NormalB rhs) []
 
-appN :: Int -> Q Exp -> Q Exp
-appN n f' = do
-  f <- f'
-  xs <- replicateM n (newName "x")
-  let args = [TupP (map VarP xs)]
-      ntup = TupE (map (AppE f . VarE) xs)
-  pure (LamE args ntup)
+appN :: Int -> String -> Q Dec
+appN n nm = do
+    let f = VarE (mkName nm)
+    xs <- replicateM n (newName "x")
+    let args = [TupP (map VarP xs)]
+        ntup = TupE (map (AppE f . VarE) xs)
+    return $ FunD (mkName nm) [Clause args (NormalB ntup) []]
 
-repN :: Int -> Q Exp -> Q Exp
-repN n = fmap TupE . replicateM n
+cmbN :: Int -> String -> Q Dec
+cmbN n nm = do
+    let f = VarE (mkName nm)
+    xs <- replicateM n (newName "x")
+    ys <- replicateM n (newName "y")
+    let args = [TupP (map VarP xs), TupP (map VarP ys)]
+        ntup = TupE (zipWith (AppE . AppE f) (map VarE xs) (map VarE ys))
+    return $ FunD (mkName nm) [Clause args (NormalB ntup) []]
+
+starIns :: Int -> Q [Dec]
+starIns n =
+    fmap pure $
+    do names <- replicateM n (newName "a")
+       let c = ConT (mkName "StarSemiring")
+           ct = map (AppT c . VarT) names
+       InstanceD Nothing ct (AppT c $ foldl AppT (TupleT n) (map VarT names)) <$>
+           sequence [appN n "star", appN n "plus"]
+
+semiringIns :: Int -> Q [Dec]
+semiringIns n =
+    fmap pure $
+    do names <- replicateM n (newName "a")
+       let c = ConT (mkName "Semiring")
+           ct = map (AppT c . VarT) names
+       InstanceD Nothing ct (AppT c $ foldl AppT (TupleT n) (map VarT names)) <$>
+           sequence [cmbN n "<+>", cmbN n "<.>", repN n "zero", repN n "one"]
